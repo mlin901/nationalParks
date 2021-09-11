918549ae513db28e65ea5347c017035d0ae4c4b3
Matt Linder <mjlinder218@gmail.com>
2021-09-11 13:25:06 -0700

    More changes to get add park functionality working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -54,42 +54,36 @@ mutation addUser($name: String!, $password: String!, $email: String!) {
 // `;
 
 // ******* NEW
-// export const SAVE_PARK = gql`
-//     mutation savePark($input: savedPark!) {
-//     savePark (input: $input)
-//         {
-//             _id
-//             name
-//             email
-//             parkCount
-//             savedParks {
-//                 parkId
-//                 parkName
-//                 description
-//                 image
-//             }
-//         }
-//     }
-// `;
-
-
-// ******* OLD
 export const SAVE_PARK = gql`
   mutation savePark($input: ParkInput!) {
     savePark(input: $input) {
-      _id
-      name
-      email
-      savedParks {
         _id
         parkId
         parkName
         description
         image
-      }
     }
   }
 `;
+
+
+// ******* OLD
+// export const SAVE_PARK = gql`
+//   mutation savePark($input: ParkInput!) {
+//     savePark(input: $input) {
+//       _id
+//       name
+//       email
+//       savedParks {
+//         _id
+//         parkId
+//         parkName
+//         description
+//         image
+//       }
+//     }
+//   }
+// `;
 
 export const REMOVE_PARK = gql`
   mutation removePark($parkId: String!) {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,7 @@
 const { gql } = require('apollo-server-express');
 
+// $$$$$------
 const typeDefs = gql`
-type Park {
-  _id: ID
-  parkId: String
-  parkName: String
-  description: String
-  image: String
-}
 
 type User {
   _id: ID
@@ -16,6 +10,14 @@ type User {
   parkCount: Int
   password: String
   savedParks: [Park]
+}
+
+type Park {
+  _id: ID
+  parkId: String
+  parkName: String
+  description: String
+  image: String
 }
 
 type Auth {
@@ -37,9 +39,52 @@ type Query {
 type Mutation {
   addUser(name: String!, email: String!, password: String!): Auth
   login(email: String!, password: String!): Auth
-  savePark(input: ParkInput): User
+  savePark(input: ParkInput): Park
   removePark(userId: ID!, parkId: ID!): User
 }
 `;
+
+// $$$$$------
+// const typeDefs = gql`
+// type Park {
+//   _id: ID
+//   parkId: String
+//   parkName: String
+//   description: String
+//   image: String
+// }
+
+// type User {
+//   _id: ID
+//   name: String
+//   email: String
+//   parkCount: Int
+//   password: String
+//   savedParks: [Park]
+// }
+
+// type Auth {
+//   token: ID!
+//   user: User
+// }
+
+// input ParkInput {
+//   parkId: String
+//   parkName: String
+//   description: String
+//   image: String
+// }
+
+// type Query {
+//   me(name: String!): User
+// }
+
+// type Mutation {
+//   addUser(name: String!, email: String!, password: String!): Auth
+//   login(email: String!, password: String!): Auth
+//   savePark(input: ParkInput): User
+//   removePark(userId: ID!, parkId: ID!): User
+// }
+// `;
 
 module.exports = typeDefs;

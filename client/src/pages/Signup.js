c9c90a0562e8e0ab6b8c8cff3c3535eab7bda708
Matt Linder <mjlinder218@gmail.com>
2021-09-12 12:42:17 -0700

    Fixed issues caused by merge

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Jumbotron } from "react-bootstrap";
 
-import { useMutation } from "@apollo/client";
+// import { useMutation } from "@apollo/client";
 
 import { useMutation } from '@apollo/react-hooks';  // *******
 

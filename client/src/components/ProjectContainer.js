b717e08cdd0ef0d62c7def2c4a1c1b6ad7dfb985
Matt Linder <mjlinder218@gmail.com>
2021-09-14 08:42:30 -0700

    Attempts to get Signup working in Heroku

--- client/src/components/ProjectContainer.js
+++ client/src/components/ProjectContainer.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-//import Search from "../pages/Search";   // *****Not used anymore????
-import SearchParks from "../pages/SearchParks"; // ******Added to use SearchParks instead of Search --->??????
+import SearchParks from "../pages/SearchParks"; 
 import Signup from "../pages/Signup";
 import Wishlist from "../pages/Wishlist";
 
@@ -13,22 +12,19 @@ export default function ProjectContainer() {
   const renderPage = () => {
     if (currentPage === "Home") {
       return <Home />;
-    }
+    };
     if (currentPage === "Login") {
       return <Login />;
-    }
-    // if (currentPage === "Search") {
-    //   return <Search />;
-    // }
-    if (currentPage === "Search") {
-      return <SearchParks />;
-    }
+    };
     if (currentPage === "Signup") {
       return <Signup />;
-    }
+    };
+    if (currentPage === "Search") {
+      return <SearchParks />;
+    };
     if (currentPage === "Wishlist") {
       return <Wishlist />;
-    }
+    };
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
